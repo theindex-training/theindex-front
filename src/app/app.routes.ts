bb140395dfa614e6831abb7aeaab9544ddb5fe7a
Anton Cholakov <antoncholakov14@gmail.com>
2026-02-08 13:46:38 +0200

    Add login page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
   },
   {
     path: '**',
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  standalone: true
+})
+export class LoginComponent {}
